c3b847251f1ac256ad5e1e4b211f24031d187717
Dinh The Loc <locdtptit@gmail.com>
2023-03-02 20:25:50 +0700

    update template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  // templateUrl: './app.component.html',
   template: `<div class="w-[1024px] mx-auto py-16">
     <h1 class="text-2xl mb-4">Single Responsibility Principle</h1>
     <main class="flex flex-row gap-4">
