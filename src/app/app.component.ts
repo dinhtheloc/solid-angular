d199e98b987ed477369e609aa2a0697aa523d968
Dinh The Loc <locdtptit@gmail.com>
2023-03-13 17:15:45 +0700

    open closed start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `<div class="w-[1024px] mx-auto py-16">
-    <h1 class="text-2xl mb-4">Single Responsibility Principle</h1>
+    <h1 class="text-2xl mb-4">Open/Closed Principle</h1>
     <main class="flex flex-row gap-4">
-      <card></card>
+      <card type="simple"></card>
+      <card type="floating"></card>
     </main>
-  </div> `,
+  </div>`,
 })
 export class AppComponent {}
